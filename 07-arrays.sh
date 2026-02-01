1afe34f0e1257e4df41056a813e5495a87fcb293
pavan <pavankumar49210@gmail.com>
2026-02-01 19:23:33 +0530

    added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,4 +2,6 @@
 
 MOVIES=("pushpa" "rrr" "devara")
 
-echo "First movie is: ${MOVIES[0]}"  
+echo "First movie is: ${MOVIES[0]}"
+echo "First movie is: ${MOVIES[1]}"
+echo "First movie is: ${MOVIES[2]}"  
