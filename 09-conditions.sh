fd6bc25c66c1dda3d565d0c70a49c7ee00d2741f
pavan <pavankumar49210@gmail.com>
2026-02-02 08:20:52 +0530

    added

---
+++ 09-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER is -gt 100 ]
+
+then 
+    echo "Given number $NUMBER is greater than 50"
+else
+    echo "Given number $NUMBER is less than or equal to 50"
+
+fi
+    
+
+
+
