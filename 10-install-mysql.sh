658bbbbbb0ee929e6e0236319bdf58d70c5979fc
pavan <pavankumar49210@gmail.com>
2026-02-02 10:46:57 +0530

    added

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -3,10 +3,16 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-
 then
     echo "ERROR:: You must have SUDO access to execute"
     exit 1
 fi 
-    dnf install mysqll -y
+ 
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "installing mysql..Failure"
+    exit 1 
+else
+    echo "install mysql..success"
  
