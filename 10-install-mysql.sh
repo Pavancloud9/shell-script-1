e18676570a2600904c0b345974db2693fd14741c
pavan <pavankumar49210@gmail.com>
2026-02-03 10:19:20 +0530

    added

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -2,37 +2,40 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR:: You must have SUDO access to execute"
-    exit 1
-fi 
-
-dnf list installed mysql  
-if [ $? -ne 0 ]   ### Not installed
-then
-    dnf install mysql -y
-    if [ $? -ne 0 ]  # 0 KI 0 EQUAL AVAKAPOTHE
+VALIDATE() {
+    if [ $1 -ne 0 ]  
     then
         echo "installing mysql..Failure"
         exit 1 
     else
         echo "install mysql..success"
     fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have SUDO access to execute"
+    exit 1
+fi 
+
+################################################
+
+dnf list installed mysql  
+if [ $? -ne 0 ]   ##### 0 KI EQUAL KAKAPOTHE
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing mysql"
 else
     echo "Mysql already installed..skipping"
 fi
   
+######################################
 
-
-# dnf list installed git
-#     if [ $? -ne 0 ]
-
-# dnf install gitt -y
-# if [ $? -ne 0 ]
-# then
-#     echo "installing git..Failure"
-#     exit 1 
-# else
-#     echo "install mysql..success"
-#  fi
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y 
+    VALIDATE $? "Installing Git"
+else
+    echo "GIT already installing...SKIPPING"
+fi
