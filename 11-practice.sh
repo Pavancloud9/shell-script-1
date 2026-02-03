a347addb9bd3ad12894c96ef5f5b978925bda9c2
pavan <pavankumar49210@gmail.com>
2026-02-03 10:05:17 +0530

    added

--- 11-practice.sh
+++ 11-practice.sh
@@ -2,6 +2,17 @@
 
 USERID=$(id -u)
 
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2..SUCCESS"
+    fi
+}
+
+
 if [ $USERID -ne 0 ]
 then
     echo "You must have sudo access to run this script"
@@ -12,27 +23,19 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then 
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then 
-        echo "Installing mysql...FAILURE"
-        exit 1
-    else
-        echo "Installing mysql..SUCCESS"
-    fi
+    VALIDATE $? "Instaling mysql"
+    
 else
     echo "Mysql already installed...SKIPPING"
 fi
+
+####################################
 
 dnf list installed git
 if [ $? -ne 0 ]
 then
     dnf install git -y
-    if [ $? -ne 0 ]
-then
-    echo "Installing Git..FAILURE"
-else
-    echo "Installing Git..SUCCESS"
-    fi
+    VALIDATE $? "Installing git"
 else
     echo "Git already installed...SKIPPING"
 fi
