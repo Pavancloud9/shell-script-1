e76a1d2ab1ebb6bb8157c581ad3f24d4b873c836
pavan <pavankumar49210@gmail.com>
2026-02-04 13:03:00 +0530

    added

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -11,7 +11,7 @@ fi
 LOGS_FOLDER_NAME="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER_NAME/$LOG_FILE/$TIMESTAMP"
+LOG_FILE_NAME="$LOGS_FOLDER_NAME/$LOG_FILE/$TIMESTAMP.log"
 
 ##########################################
 
