53d62aed989594cfa4e4bea7c2815bf5a0d5818a
pavan <pavankumar49210@gmail.com>
2026-02-08 11:21:14 +0530

    added

---
+++ 13-delete-old-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have SUDO access to execute"
+    exit 1
+fi 
+
+SOURCE_DIRECTORY="/home/ec2-user/shell-script-1/app-logs"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"  
+ 
+
+##########################################
+
+PACKAGE_FUNCTION () {
+    if [ $1 -ne 0 ]  
+    then
+        echo "$2..Failure"
+        exit 1 
+    else
+        echo "$2..success"
+    fi
+}
+
+find $SOURCE_DIRECTORY -name "*.log" -mtime +14
