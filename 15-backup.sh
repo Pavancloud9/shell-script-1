86cf7ae133b899e267224b42650ce9852c7ef253
pavan <pavankumar49210@gmail.com>
2026-02-08 19:42:54 +0530

    added

---
+++ 15-backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"  
+########    10-install-mysql-2026-02-04_12-45-43.log 
+
+##########################################
+
+PACKAGE () {
+    if [ $1 -ne 0 ]  
+    then
+        echo "$2..Failure"
+        exit 1 
+    else
+        echo "$2..success"
+    fi
+}
+
+USAGE() {
+  echo "USAGE of the script is: sh 15-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+}
+
+################################################
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+if [ $# -lt 2 ]
+    then
+        USAGE
+    fi
