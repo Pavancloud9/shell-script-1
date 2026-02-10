b6dbd33500c31f005055027a01a557652932980c
pavan <pavankumar49210@gmail.com>
2026-02-10 19:36:45 +0530

    added

--- 15-backup.sh
+++ 15-backup.sh
@@ -59,7 +59,7 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS_AGO)
 # ./may.log
 # ./nov.log
 
-if [ -n $FILES ] ## This condition Meaning is FILES are there (-n means not empty)
+if [ -n $FILES ] 
 then
     echo "Files are: $FILES"
 else
