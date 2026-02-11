9f4160fc17c4cc3bdaf9b407ce98a7eb2cc9bbf1
pavan <pavankumar49210@gmail.com>
2026-02-11 19:39:29 +0530

    added

--- 15-backup.sh
+++ 15-backup.sh
@@ -63,7 +63,7 @@ if [ -n "$FILES" ]
 then
     echo "Files are: $FILES"
       ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-      find $SOURCE_DIR -name "*.log" -mtime $DAYS_AGO | zip -@ $ZIP_FILE
+      find $SOURCE_DIR -name "*.log" -mtime +$DAYS_AGO | zip -@ $ZIP_FILE
         if [ $? -eq 0 ]
         then
             echo "Zip file created successfully for files older than $DAYS_AGO"
