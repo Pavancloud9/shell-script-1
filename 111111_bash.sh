701c9a88230576d9e9144e74605ca669bb0fe603
pavan <pavankumar49210@gmail.com>
2026-02-18 20:00:19 +0530

    added

--- 111111_bash.sh
+++ 111111_bash.sh
@@ -45,7 +45,7 @@ then
 fi
 
 FILES=$(find $SOURCE_DIR -type f -name "*.java" -mtime +14)
-if [ -n $FILES ] ### -n means not empty
+if [ -n "$FILES" ] ### -n means not empty
 then
     echo "Files are: $FILES"
     ZIP_FILE_DIRECTORY="$DEST_DIR/app-logs-$TIMESTAMP.zip" | zip -r
